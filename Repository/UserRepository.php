5e52239fcc65f1e2a518e6310509f9de092d578f
yimmit303 <timsteiner303@gmail.com>
2018-03-12 17:42:57 -0400

    More User changes

--- Repository/UserRepository.php
+++ Repository/UserRepository.php
@@ -3,30 +3,43 @@ require("Repository.php");
 require_once(realpath(dirname(__FILE__) . '/../Models/User.php'));
 class UserRepository extends Repository
 {
-    function isValidUser()
-    {
-        
-    }
-    function doesUserExist($username_to_check)
+    function isValidUser($username_to_check)
     {
         $sql = "SELECT username FROM users";
         $this->conn->query($sql);
+        $result = $this->conn->query($sql);
+        $isValid = TRUE;
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
                 if ($row["username"] == $username_to_check){
-                    return TRUE;
-                }
-                
-                else{
-                    return FALSE;
+                    $isValid = FALSE;
                 }
             }
         } 
         else {
             echo "0 results";
         }
+        return $isValid;
     }
-    function getAllUserInfo()
+    function doesUserExist($username_to_check)
+    {
+        $sql = "SELECT username FROM users";
+        $this->conn->query($sql);
+        $result = $this->conn->query($sql);
+        $doesExist = FALSE;
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                if ($row["username"] == $username_to_check){
+                    $doesExist = TRUE;
+                }
+            }
+        } 
+        else {
+            echo "0 results";
+        }
+        return $doesExist;
+    }
+    function getInfoByID($id)
     {
         $sql = "SELECT userId, username, password, firstName, lastName, dateOfBirth, bio, interest, job, employeer, isSuspended, isPublic, profilePicture FROM users";
         $result = $this->conn->query($sql);
@@ -34,25 +47,44 @@ class UserRepository extends Repository
             // output data of each row
             $user = new User();
             while($row = $result->fetch_assoc()) {
-                $user->UserId = $row["userId"];
-                $user->Username = $row["username"];
-                $user->Password = $row["password"];
-                $user->FirstName = $row["firstName"];
-                $user->LastName = $row["lastName"];
-                $user->DateOfBirth = $row["dateOfBirth"];
-                $user->Bio = $row["bio"];
-                $user->Interest = $row["interest"];
-                $user->Job = $row["job"];
-                $user->Employer = $row["employeer"];
-                $user->isSuspended = $row["isSuspended"];
-                $user->isPublic = $row["isPublic"];
-                $user->ProfilePicture = $row["profilePicture"];
+                if($row["userId"] == $id)
+                {
+                    $user->UserId = $row["userId"];
+                    $user->Username = $row["username"];
+                    $user->Password = $row["password"];
+                    $user->FirstName = $row["firstName"];
+                    $user->LastName = $row["lastName"];
+                    $user->DateOfBirth = $row["dateOfBirth"];
+                    $user->Bio = $row["bio"];
+                    $user->Interest = $row["interest"];
+                    $user->Job = $row["job"];
+                    $user->Employer = $row["employeer"];
+                    $user->isSuspended = $row["isSuspended"];
+                    $user->isPublic = $row["isPublic"];
+                    $user->ProfilePicture = $row["profilePicture"];
+                }
             }
             return $user;
         } else {
             echo "0 results";
         }
 
+    }
+    function getIDByUser($username)
+    {
+        $sql = "SELECT userId, username FROM users";
+        $this->conn->query($sql);
+        $result = $this->conn->query($sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                if ($row["username"] == $username){
+                    return $row["userId"];
+                }
+            }
+        } 
+        else {
+            echo "0 results";
+        }
     }
 }
 ?>
--- Views/UserPage.php
+++ Views/UserPage.php
@@ -1,7 +1,7 @@
 <?php
 require_once("../Repository/UserRepository.php");
 $userRepo = new UserRepository();
-$user = $userRepo->getAllUserInfo();
+$user = $userRepo->getInfoByID(2);
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,7 +14,12 @@ $user = $userRepo->getAllUserInfo();
 </head>
 <body>
     <?php
-        echo $user->Username;
+        echo "Username: ".$user->Username."<br />";
+        echo "Password: ".$user->Password."<br />";
+        echo "Does the user foo exist? ".($userRepo->doesUserExist("foo") ? "TRUE" : "FALSE")."<br />";
+        echo "Would the username user be valid? ".($userRepo->isValidUser("user") ? "TRUE" : "FALSE")."<br />";
+        echo "Would the username raboof be valid? ".($userRepo->isValidUser("raboof") ? "TRUE" : "FALSE")."<br />";
+        echo "What is the id of user foo? ".$userRepo->getIDByUser("foo")."<br />";
     ?>
 </body>
 </html>
