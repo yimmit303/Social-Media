c6cb22527854de39115e719f36c67c29388281e4
yimmit303 <timsteiner303@gmail.com>
2018-03-17 22:19:53 -0400

    Fully complete search functionality

--- Views/AddUser.php
+++ Views/AddUser.php
@@ -4,6 +4,8 @@
     #$didSucceed = $userRepo->addUserBaseInfo("user","pass", "foo", "bar");
     #$didSucceed = $userRepo->addUserBaseInfo("mike", "wazoski", "your", "paperwork");
     #echo "added user : ".($didSucceed ? "TRUE" : "FALSE");
-    $userRepo->searchUser("mike ashkjhj sadasdas");
+    $userRepo->searchUser("y");
+    $user = $userRepo->getInfoByID(1);
+    echo $user->Username;
 
 ?>
